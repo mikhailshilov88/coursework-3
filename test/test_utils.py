cbf1e80f5f0de7efbe12b0d79b306162d20831e8
Mihail <mikhailshilov@yandex.ru>
2023-04-29 22:24:17 +0500

    dorabotka test

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,24 +1,24 @@
-from utils import filter_sort, load_data, rormat_date, mask_card
+from utils import filter_sort, load_data, format_date, mask_card, formatted_data
 
 
 def test_load_data():
     list_ = [
-  {
-    "id": 441945886,
-    "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-      "amount": "31957.58",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
-  }
-]
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        }
+    ]
     assert load_data('test_json') == list_
 
 
@@ -47,11 +47,6 @@ def test_filter_sort():
             'date': '2020-11-29T07:18:23.941293'
         },
         {
-            'id': 2,
-            'state': 'OPEN',
-            'date': '2019-11-29T07:18:23.941293'
-        },
-        {
             'id': 1,
             'state': 'EXECUTED',
             'date': '2018-11-29T07:18:23.941293'
@@ -59,12 +54,53 @@ def test_filter_sort():
     ]
     assert filter_sort(list_) == sorted_list
 
+
 def test_format_date():
-    assert format_date('2019-08-26T10:50:58.294041') == '03.07.2019'
-    assert format_date('2019-08-26T10:50:58.294041') == '19.08.2018'
+    assert format_date('2019-07-03T18:35:29.512364') == '03.07.2019'
+    assert format_date('2018-08-19T04:27:37.904916') == '19.08.2018'
+
 
 def test_mask_card():
-    assert mask_card("Счет 64686473678894779589") == 'Счет **5355'
+    assert mask_card("Счет 10848359769870775355") == 'Счет **5355'
     assert mask_card("Visa Classic 6831982476737658") == 'Visa Classic 6831 98** **** 7658'
     assert mask_card("Maestro 3928549031574026") == 'Maestro 3928 54** **** 4026'
 
+
+def test_formatted_data():
+    dict1 = {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    }
+    dict2 = {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "to": "Счет 64686473678894779589"
+    }
+    str1 = '26.08.2019 Перевод организации\n' \
+           'Maestro 1596 83** **** 5199 -> Счет **9589\n' \
+           '31957.58 руб.\n'
+    str2 = '26.08.2019 Перевод организации\n' \
+           'Счет **9589\n' \
+           '31957.58 руб.\n'
+    assert formatted_data(dict1) == str1
+    assert formatted_data(dict2) == str2
